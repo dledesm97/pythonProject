a888d74e74a12c6431be28463c7320941ec7883e
David <dledesm@users.noreply.github.com>
2022-03-09 17:52:23 +0000

    Added a distance function

--- travel.py
+++ travel.py
@@ -1,45 +1,10 @@
 from googletrans import Translator
+from geopy.distance import geodesic
 import language
 import os
 
 language = language.LANGUAGES
 translator = Translator()
-
-def homepage():
-    print("{:-^30}".format("-"))
-    print("{:^30}".format("Where are you traveling?"))
-    print("{:-^30}".format("-"))
-
-    destination = input("Enter the location?")
-    os.system('clear')
-
-def titles():
-    #os.system('clear')
-    print("{:-^30}".format("-"))
-    print("{:^30}".format("Simple Translation Program"))
-    print("{:-^30}".format("-"))
-
-def language_title():
-    print("{:-^30}".format("-"))
-    print("{:^30}".format("Available Languages"))
-    print("{:-^30}".format("-"))
-
-def all_symbol():
-    for key, value in language.items():
-        print(key, ':', value)
-
-
-def pair_language():
-    texts = input("Text to translate: ")
-    destination = (input("Destination language: ")).lower()
-
-    try:
-        translate = translator.translate(texts, dest=destination)
-        print("\a")
-        print("Translated text:", translate.text)
-    except:
-        print("Language destination not exist")
-
 
 def menu():
     print("Main Program")
@@ -61,12 +26,69 @@ def menu():
             language_title()
             all_symbol()
         elif choose == "3":
-            print("Thanks for using")
+            print("Have a good day!")
             exit()
         else:
             print("Choice not found")
 
+def homepage():
+    print("{:-^30}".format("-"))
+    print("{:^30}".format("Where are you traveling?"))
+    print("{:-^30}".format("-"))
+    
+    destination = input("Enter the location: ")
+    os.system('clear')
+    
+    print("1. Translator")
+    print("2. Find Distance Between Locations")
+    userInput = int(input("Your choice: "))
+    if userInput == 1:
+        menu()
+    if userInput == 2:
+        find_distance()
+    
+def titles():
+    #os.system('clear')
+    print("{:-^30}".format("-"))
+    print("{:^30}".format("Let me help you translate"))
+    print("{:-^30}".format("-"))
+    
+def language_title():
+    print("{:-^30}".format("-"))
+    print("{:^30}".format("Available Languages"))
+    print("{:-^30}".format("-"))
+    
+def all_symbol():
+    for key, value in language.items():
+        print(key, ':', value)
+    
+    
+def pair_language():
+    texts = input("Text to translate: ")
+    destination = (input("Destination language: ")).lower()
+    
+    try:
+        translate = translator.translate(texts, dest=destination)
+        print("\a")
+        print("Translated text:", translate.text)
+    except:
+        print("Language destination not exist")
 
-if __name__ == '__main__':
-    titles()
-    menu()
+def find_distance():
+    print("-----Your Location------")
+    location_la = float(input("Enter the latitude: "))
+    location_lo = float(input("Enter the longitude: "))
+    location = (location_la, location_lo)
+    print("-----Your Destination-----")
+    destination_la = float(input("Enter the latitude: "))
+    destination_lo = float(input("Enter the longitude: "))
+    destination = (destination_la,destination_lo)
+    print("-----Destinace Between In Miles-----")
+    print(geodesic(location,destination).miles)
+
+
+#if __name__ == '__main__':
+   #homepage()
+   #titles()
+   #menu()
+homepage()
