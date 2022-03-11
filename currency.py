6311bc3d81642bfe1f3f4b8309c06946c8bf370c
David <dledesm@users.noreply.github.com>
2022-03-11 15:51:37 +0000

    added comments and formatted the output

--- currency.py
+++ currency.py
@@ -1,24 +1,27 @@
 from currency_converter import CurrencyConverter
-import currencies
+import currencies   #importing currencies file
 import travel
 
 output = currencies.CURRENCIES
 def currency_converter():
     try:
         while True:
+            print("{:-^30}".format("-"))
+            #listing out the available options when converting
             print("1. Exchange")
             print("2. List of Currencies")
             print("3. Exit Program")
+            print("{:-^30}".format("-"))
             user_input = input("Make your selection: ")
             if user_input == "1":
                 counter = 0
-                c = CurrencyConverter()
+                c = CurrencyConverter() #creating an instance of currency converter
                 print("Exchange Your Currency")
-                current =(input("Current Currency: ")).upper()
+                current =(input("Current Currency: ")).upper() #the currency must be all uper-case
                 for x in output:
                     if current == x:
-                        counter += 1
-                        continue
+                        counter += 1            # checking if the currency that was inputted is in the 
+                        continue                # list of availble currencies    
                 future = (input("Expected Currency: ")).upper()
                 for y in output:
                     if future == y:
@@ -29,9 +32,12 @@ def currency_converter():
                     currency_converter()
                 amount = float(input("How much do you want to exchange? "))
                 exchanged_amount = c.convert(amount, current, future)
+                print("{:-^30}".format("-"))
                 print(f"{amount} {current} = {exchanged_amount} {future}")
             elif user_input == "2":
-                print(output)
+                print("{:-^30}".format("-"))
+                print("{:^30}".format("List Of Currencies"))
+                print(', '.join(output))
                 currency_converter()
             elif user_input == "3":
                 exit()
