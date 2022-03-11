bb8fb8828f1dfee96e46f0f86e9f7a8c31532285
David <dledesm@users.noreply.github.com>
2022-03-11 06:53:14 +0000

    Added additional functionality

--- currency.py
+++ currency.py
@@ -1,19 +1,41 @@
 from currency_converter import CurrencyConverter
 import currencies
+import travel
 
 output = currencies.CURRENCIES
 def currency_converter():
-    print("1. Exchange")
-    print("2. List of Currencies")
-    user_input = int(input("Make your selection: "))
-    if user_input == 1:
-        c = CurrencyConverter()
-        print("Exchange Your Currency")
-        current = (input("Current Currency: ")).upper()
-        future = (input("Expected Currency: ")).upper()
-        amount = float(input("How much do you want to exchange? "))
-        exchanged_amount = c.convert(amount, current, future)
-        print(f"{amount} {current} = {exchanged_amount} {future}")
-    if user_input == 2:
-        print(output)
-        currency_converter()
+    try:
+        while True:
+            print("1. Exchange")
+            print("2. List of Currencies")
+            print("3. Exit Program")
+            user_input = input("Make your selection: ")
+            if user_input == "1":
+                counter = 0
+                c = CurrencyConverter()
+                print("Exchange Your Currency")
+                current =(input("Current Currency: ")).upper()
+                for x in output:
+                    if current == x:
+                        counter += 1
+                        continue
+                future = (input("Expected Currency: ")).upper()
+                for y in output:
+                    if future == y:
+                        counter += 1
+                        continue
+                if counter < 2:
+                    print("Input availabe currencies")
+                    currency_converter()
+                amount = float(input("How much do you want to exchange? "))
+                exchanged_amount = c.convert(amount, current, future)
+                print(f"{amount} {current} = {exchanged_amount} {future}")
+            elif user_input == "2":
+                print(output)
+                currency_converter()
+            elif user_input == "3":
+                exit()
+            else:
+                print("Choice not found")
+    except KeyboardInterrupt:
+        print("Exiting")
--- travel.py
+++ travel.py
@@ -9,47 +9,53 @@ import os
 destination = countries.COUNTRIES
 
 def homepage():
-    print("{:-^100}".format("-"))
-    print("{:^100}".format("Where are you traveling?"))
-    print("{:-^100}".format("-"))
+    try:
+        print("{:-^100}".format("-"))
+        print("{:^100}".format("Where are you traveling?"))
+        print("{:-^100}".format("-"))
 
-    #display all the available countries for this program
-    print(' '.join(destination))
+        #display all the available countries for this program
+        print(' '.join(destination))
 
-    #asking for user input and making it all uppercase when storing it in the variable
-    country = (input("\nFrom the options above, please type your destination: ")).upper()
-    # keeping track of the user input and looping through the countries list
-    # to make sure it matches with at least one country if NOT ask for user input again
-    counter = 0
-    for x in destination:
-        if country == x:
-            counter = 1
-            selection(country)
-    if (counter == 0):
-        print("Please enter a valid country")
-        homepage()
-        
-    
-    #os.system('clear')
+        #asking for user input and making it all uppercase when storing it in the variable
+        country = (input("\nFrom the options above, please type your destination: ")).upper()
+        # keeping track of the user input and looping through the countries list
+        # to make sure it matches with at least one country if NOT ask for user input again
+        counter = 0
+        for x in destination:
+            if country == x:
+                counter = 1
+                selection(country)
+        if (counter == 0):
+            print("Please enter a valid country")
+            homepage()
+    except KeyboardInterrupt:
+        print("Exiting Program")
 
 # Outputting the list of options that the user can do in this program
 def selection(country):
-    print("1. Translator")
-    print("2. Find Distance Between Locations")
-    print("3. Quick Facts")
-    print("4. Currency Exchange")
-    print("5. Exit Program")
-    userInput = int(input("Your choice: "))
-    if userInput == 1:
-        translator.menu()   #this function is in the translator.py file and we are calling it from here 
-    if userInput == 2:
-        distance.find_distance()    #calling the find_distance function from the distance file
-    if userInput == 3:
-        information.quick_facts(country)
-    if userInput == 4:
-        currency.currency_converter()
-    if userInput == 5:
-        exit()              # killing the program if the user enters 5
+    while True:
+        try:
+            print("1. Translator")
+            print("2. Find Distance Between Locations")
+            print("3. Quick Facts")
+            print("4. Currency Exchange")
+            print("5. Exit Program")
+            userInput = input("Your choice: ")
+            if userInput == "1":
+                translator.menu()   #this function is in the translator.py file and we are calling it from here 
+            elif userInput == "2":
+                distance.find_distance()    #calling the find_distance function from the distance file
+            elif userInput == "3":
+                information.quick_facts(country)
+            elif userInput == "4":
+                currency.currency_converter()
+            elif userInput == "5":
+                exit()              # killing the program if the user enters 5
+            else:
+                print("Choice not found")
+        except KeyboardInterrupt:
+            print("Exiting Program")
         
 if __name__ == '__main__':
    homepage()
